a2746ef2783c0f6b743c4b491f1cde8764209bc3
Damodhar <jdamodhar@deloitte>
2022-09-13 11:16:44 +0530

    ping pong done

--- day-22/main.py
+++ day-22/main.py
@@ -1,12 +1,13 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 #TODO: Create a screen
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width = 800, height = 600)
+screen.setup(width = 800, height = 620)
 screen.title("Pong")
 screen.tracer(0)
 
@@ -24,20 +25,32 @@ screen.onkey(l_paddle.go_up, 'w')
 screen.onkey(l_paddle.go_down, "s")
 
 #TODO: Create the ball and make it move
-
 ball = Ball()
+#TODO: Keep score
+score = Scoreboard()
 
 while True:
     screen.update()
     ball.move()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     #TODO: Detect collision with wall and bounce
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
     
-#TODO: Detect collision with paddle
-#TODO: Detect when paddle misses
-#TODO: Keep score
+    #TODO: Detect collision with paddle
+    if ball.distance(r_paddle) < 60 and ball.xcor() > 320 or ball.distance(l_paddle) < 60 and ball.xcor() < -320:
+        ball.bounce_x()
+ 
+        
+    #TODO: Detect when paddle misses
+    if ball.xcor() > 360 :
+        ball.reset_position()
+        score.l_point()
+        
+    if ball.xcor() < -360:
+        ball.reset_position()
+        score.r_point()
+        
 
 
 
--- day-22/paddle.py
+++ day-22/paddle.py
@@ -10,10 +10,10 @@ class Paddle(Turtle):
         self.goto(x,y)
 
     def go_up(self):
-        new_y= self.ycor() + 20
+        new_y= self.ycor() + 60
         self.goto(self.xcor(), new_y)
     
     def go_down(self):
-        new_y= self.ycor() - 20
+        new_y= self.ycor() - 60
         self.goto(self.xcor(), new_y)
 
