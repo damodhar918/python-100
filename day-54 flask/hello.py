f1222ca30c8117e6ee4f3f1878711469d805c45e
Damodhar <jdamodhar@deloitte>
2022-09-20 10:07:27 +0530

    flask 1

--- day-54 flask/hello.py
+++ day-54 flask/hello.py
@@ -1,11 +1,39 @@
 from flask import Flask
+from random import randint
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-from flask import render_template
+msg = ['Too low, try again!', 'Too high, try again!', 'You found me!']
+links = ['https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif',
+         'https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif',
+         'https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif',]
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+
+def render(case):  # the rendering function with random RGB colors
+    return f'<h1 style="color: #{randint(0, 0xFFFFFF):06x}">{msg[case]}</h1>' \
+           f'<img src="{links[case]}" width=300 alt="{msg[case]}">'
+
+
+@app.route('/')
+def home():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" width=300 alt="numbers">'
+
+
+number = randint(0, 9)
+
+
+@app.route('/<int:guess_num>')
+def compare(guess_num):
+    global number
+    if guess_num < number:
+        return render(0)
+    elif guess_num > number:
+        return render(1)
+    elif guess_num == number:
+        number = randint(0, 9)  # change the number when it is guessed
+        return render(2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ day-54 flask/hello1.py
@@ -0,0 +1,59 @@
+
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
+from flask import render_template
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
